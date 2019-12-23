62607d0c2dbb49e7d3651a2b9fe854c899c47bc9
Egon Elbre <egonelbre@gmail.com>
2019-12-23 21:45:04 +0200

    update script

--- service/genproto.sh
+++ service/genproto.sh
@@ -1,1 +1,2 @@
-protoc -I `pwd`:`pwd`/../common --go_out=plugins=gRPC,paths=source_relative:`pwd` `pwd`/pbi/service.proto
+COMMONDIR=`go list -f "{{ .Dir }}" -m github.com/egonelbre/prototest/common`
+protoc -I `pwd`:$COMMONDIR --go_out=plugins=gRPC,paths=source_relative:`pwd` `pwd`/pbi/service.proto
--- service/go.mod
+++ service/go.mod
@@ -2,4 +2,7 @@ module github.com/egonelbre/prototest/service
 
 go 1.13
 
-require github.com/egonelbre/prototest/common v0.0.0-20191223192134-b8630d3f3818
+require (
+	github.com/egonelbre/prototest/common v0.0.0-20191223193046-4f7801bca5ea
+	github.com/golang/protobuf v1.3.2
+)
